393a838aef7ed84a6ea63641a375e25599ba0212
Shaun Hevey <shaun@hevey.co>
2015-12-14 01:38:49 +1100

    added stuff

--- istaflow.py
+++ istaflow.py
@@ -3,11 +3,49 @@ from views import ElementListView
 from managers import ElementManager
 import ui
 
+
+
+class ista(object):
+	def __init__(self):
+		self.elements_view = None
+		self.element_manager = None
+		self.elements = None
+		self.setup_elementsmanager()		
+		self.get_valid_elements()
+		self.setup_elementsview()
+	
+	def get_valid_elements(self):
+		if self.element_manager == None:
+			raise ValueError("element_manager hasnt been initialised")
+		else:	
+			self.elements = self.element_manager.get_all_elements(type='valid')
+			
+	def setup_elementsmanager(self):
+		self.element_manager = ElementManager.ElementManager()
+				
+	def setup_elementsview(self):
+		self.elements_view = ElementListView.get_view(self.elements, self.cb)
+	
+	def show_elementsview(self):
+		if self.elements_view == None:
+			raise ValueError("elements_view hasnt been initialised")
+		else:	
+			self.elements_view.present()	
+			
+	def close_elementsview(self):
+		if self.elements_view == None:
+			raise ValueError("elements_view hasnt been initialised")
+		else:	
+			self.elements_view.close()
+			
+	def cb(self, element):
+		print element.get_title()
+		self.close_elementsview()
+
+
 def main():
-	element_manager = ElementManager.ElementManager()
-	elements = element_manager.get_all_elements(type='valid')
-	element_view = ElementListView.get_view(elements)
-	element_view.present()	
+	m = ista()
+	m.show_elementsview()
 	
 if __name__ == '__main__':
 	main()
--- views/ElementListView.py
+++ views/ElementListView.py
@@ -1,11 +1,12 @@
 # coding: utf-8
 import ui
 class ElementListView(object):
-	def __init__(self, elements):
+	def __init__(self, elements, selectedCallBack):
 		self.elements = elements
+		self.scb = selectedCallBack
 
 	def tableview_did_select(self, tableview, section, row):
-		pass
+		self.scb(self.elements[row])
 		
 	def tableview_title_for_header(self, tableview, section):
 		return 'Elements'
@@ -20,11 +21,12 @@ class ElementListView(object):
 		cell = ui.TableViewCell('subtitle')
 		cell.text_label.text = self.elements[row].get_title()
 		cell.detail_text_label.text = self.elements[row].get_description()
+		cell.image_view = ui.ImageView().image = 'iob:bag_256'
 		cell.selectable = True
 		return cell
 
-def get_view(elements):
-	dbo = ElementListView(elements = elements)
+def get_view(elements, cb):
+	dbo = ElementListView(elements = elements, selectedCallBack = cb)
 	table_view = ui.TableView()
 	table_view.name = 'Elements'
 	table_view.data_source = dbo
