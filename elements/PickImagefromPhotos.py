3580be41a83cbc999a1d5ec426ea3dc4a3b3a553
Shaun Hevey <shaun@hevey.co>
2015-12-21 22:19:06 +1100

    create pick image for photos element

---
+++ elements/PickImagefromPhotos.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+from ElementBase import ElementBase
+import photos
+
+class PickImagefromPhotos(ElementBase):
+	def __init__(self):
+		self.status = 'running'
+		self.output = None 
+		self.params = {}
+	
+	def get_status(self):
+		return self.status
+		
+	def get_input_type(self):
+		return None
+	
+	def get_output(self):
+		return self.output
+		
+	def get_output_type(self):
+		return 'image'
+		
+	def get_params(self):
+		return self.params
+		
+	def set_params(self, params = {}):
+		self.params = params
+		
+	def get_description(self):
+		return 'Pick a image from photo library and returns it.'
+	
+	def get_title(self):
+		return 'Pick Image from Photos'
+		
+	def get_icon(self):
+		return 'iob:camera_32'
+		
+	def get_category(self):
+		return 'Image'
+	
+	def run(self):
+		return photos.pick_image()
