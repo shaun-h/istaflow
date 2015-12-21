f8ae69fe25cb47f795fb73652d6061e5c11f2744
Shaun Hevey <shaun@hevey.co>
2015-12-21 22:19:31 +1100

    Created save image to camera roll element

---
+++ elements/SaveImagetoCameraRoll.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+from ElementBase import ElementBase
+import photos
+
+class SaveImagetoCameraRoll(ElementBase):
+	def __init__(self):
+		self.status = 'running'
+		self.output = None 
+		self.params = {}
+	
+	def get_status(self):
+		return self.status
+		
+	def get_input_type(self):
+		return 'image'
+	
+	def get_output(self):
+		return self.output
+		
+	def get_output_type(self):
+		return None
+		
+	def get_params(self):
+		return self.params
+		
+	def set_params(self, params = {}):
+		self.params = params
+		
+	def get_description(self):
+		return 'Saves input image to Camera Roll'
+	
+	def get_title(self):
+		return 'Save Image to Camera Roll'
+		
+	def get_icon(self):
+		return 'iob:camera_32'
+		
+	def get_category(self):
+		return 'Image'
+
+	def run(self, input):
+		photos.save_image(input)
+		self.status = 'complete'
