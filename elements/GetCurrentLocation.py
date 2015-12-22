68501f6c4467e560e124bc4a3426883afd088ecb
Shaun Hevey <shaun@hevey.co>
2015-12-22 23:36:53 +1100

    Created GetCurrentLocation element

---
+++ elements/GetCurrentLocation.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+from ElementBase import ElementBase
+import location
+
+class GetCurrentLocation(ElementBase):
+	def __init__(self):
+		self.status = 'running'
+		self.output = None 
+		self.params = {}
+	
+	def get_status(self):
+		return self.status
+		
+	def get_input_type(self):
+		return None
+	
+	def get_output(self):
+		return self.output
+		
+	def get_output_type(self):
+		return 'location'
+		
+	def get_params(self):
+		return self.params
+		
+	def set_params(self, params = {}):
+		self.params = params
+		
+	def get_description(self):
+		return 'Get current location of the device'
+	
+	def get_title(self):
+		return 'Get Current Location'
+		
+	def get_icon(self):
+		return 'iob:location_32'
+		
+	def get_category(self):
+		return 'Location'
+	
+	def run(self, input=''):
+		location.start_updates()
+		loc = location.get_location()
+		location.stop_updates()
+		return loc
