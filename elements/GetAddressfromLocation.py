d35bd415bdac9cfa496c90b7b027d05f45055e18
Shaun Hevey <shaun@hevey.co>
2015-12-23 19:05:04 +1100

    Created GetAddressfromLocation element

---
+++ elements/GetAddressfromLocation.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+from ElementBase import ElementBase
+import location
+
+class GetAddressfromLocation(ElementBase):
+	def __init__(self):
+		self.status = 'running'
+		self.output = None 
+		self.params = {}
+	
+	def get_status(self):
+		return self.status
+		
+	def get_input_type(self):
+		return 'location'
+	
+	def get_output(self):
+		return self.output
+		
+	def get_output_type(self):
+		return 'address'
+		
+	def get_params(self):
+		return self.params
+		
+	def set_params(self, params = {}):
+		self.params = params
+		
+	def get_description(self):
+		return 'Get address from location passed in.'
+	
+	def get_title(self):
+		return 'Get Address from Location'
+		
+	def get_icon(self):
+		return 'iob:location_32'
+		
+	def get_category(self):
+		return 'Location'
+	
+	def run(self, input):
+		self.status = 'complete'
+		return location.reverse_geocode(input)
