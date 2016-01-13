01aa975a03664fba32100676b33ae42542ed2ba2
Shaun Hevey <shaun@hevey.co>
2016-01-14 00:50:51 +1100

    Created openlocationingooglemaps

---
+++ elements/OpenLocationinGoogleMaps.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+from ElementBase import ElementBase
+from ElementParameter import ElementParameter
+from ElementValue import ElementValue
+from  objc_util import *
+import console
+
+class OpenLocationinGoogleMaps(ElementBase):
+	def __init__(self):
+		self.status = 'running'
+		self.output = None 
+		self.params = None
+		self.setup_params()
+	
+	def setup_params(self):
+		pass
+	
+	def get_status(self):
+		return self.status
+		
+	def get_input_type(self):
+		return 'location'
+	
+	def get_output(self):
+		return self.output
+		
+	def get_output_type(self):
+		return None
+		
+	def get_params(self):
+		return self.params
+		
+	def set_params(self, params = []):
+		self.params = params
+		
+	def get_description(self):
+		return 'Opens a location in the Google Maps app'
+	
+	def get_title(self):
+		return 'Open Location in Google Maps'
+		
+	def get_icon(self):
+		return 'iob:map_32'
+		
+	def get_category(self):
+		return 'External App'
+	
+	def run(self, input=''):
+		url = 'comgooglemaps://?center=' + str(input.value['latitude'])+','+str(input.value['longitude'])
+		uia = ObjCClass('UIApplication').sharedApplication()
+		if not uia.openURL_(nsurl(url)):
+			console.alert(title='Error oppening App',message='Is Google Maps app installed?',button1='Ok',hide_cancel_button=True)
+		self.status = 'complete'
