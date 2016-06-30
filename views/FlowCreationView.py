e76a3da5279760b3ad755f301c03e37f506a5964
Shaun Hevey <shaun@hevey.co>
2016-06-30 23:55:49 +1000

    made some changes to flowcreationview

--- views/FlowCreationView.py
+++ views/FlowCreationView.py
@@ -10,23 +10,23 @@ class FlowCreationView(object):
 		self.flowType = flowType
 		self.elements = elements
 		self.saveCallBack = saveCallBack
+		self.safeFlowAction = saveFlowAction
 		self.flowTypeSelection = flowTypeSelection
 		self.showElementRuntimeView = showElementRuntimeView
-		self.extraRows = 3
+		self.extraRows = 4
 		self.adminRow = 0
-		self.saveToHomeScreenRow = 1
+		self.saveToHomeScreenRow = 3
 		self.typeRow = 2
+		self.titleRow = 1
 		self.title = ''
 		self.oldtitle = ''
 		self.currentElementNumber = -1
 		self.addElementButton = ui.ButtonItem(title = 'Add Element', action = addElementAction)
-		self.saveFlowButton = ui.ButtonItem(title='Save', action=saveFlowAction)
+		self.saveFlowButton = ui.Button(title='Save')
 		self.runFlowButton = ui.ButtonItem(title='Run', action=runFlowAction)
-		
-		self.titleButton = ui.Button(title='Change Title')
 		self.saveToHomeScreenAction = saveToHomeScreenAction
 		self.editButtonsRight = [self.addElementButton]
-		self.editButtonsLeft = [self.saveFlowButton]
+		self.editButtonsLeft = []
 		self.runButtonsRight = [self.runFlowButton]
 		self.runButtonsLeft = []
 		self.thememanager = thememanager
@@ -38,29 +38,37 @@ class FlowCreationView(object):
 			show_run_buttons()
 			
 	def tableview_did_select(self, tableview, section, row):
-		if row >= self.extraRows:
-			element = self.elements[row-self.extraRows]
+		if section == 0:
+			if row == self.typeRow:
+				self.flowTypeSelection()
+			elif row == self.saveToHomeScreenRow:
+				self.saveToHomeScreenAction()
+			elif row == self.titleRow:
+				self.change_title()
+		elif section == 1:
+			element = self.elements[row]
 			params = element.get_params() or []
 			show = any(p.display for p in params)
 			if show:
 				self.showElementRuntimeView(element)
-		elif row == self.typeRow:
-			self.flowTypeSelection()
-		elif row == self.saveToHomeScreenRow:
-			self.saveToHomeScreenAction()
 			
 	def tableview_title_for_header(self, tableview, section):
-		pass
+		if section == 1:
+			return 'Elements'
 
 	def tableview_number_of_sections(self, tableview):
-		return 1
+		return 2
 
 	def tableview_number_of_rows(self, tableview, section):
-		return len(self.elements)+self.extraRows
+		if section == 0:
+			return self.extraRows
+		elif section == 1:
+			return len(self.elements)
 		
 	def tableview_cell_for_row(self, tableview, section, row):
-		if row >= self.extraRows:
-			element = self.elements[row-self.extraRows]
+		#if row >= self.extraRows:
+		if section == 1:
+			element = self.elements[row]
 			cell = ui.TableViewCell('subtitle')
 			cell.selectable = False
 			cell.text_label.text = element.get_title()
@@ -76,7 +84,7 @@ class FlowCreationView(object):
 			cell.selectable = selectable
 			if self.currentElementNumber >= self.extraRows:
 				cell.selectable = False
-			if self.currentElementNumber+2== row:
+			if self.currentElementNumber+(-1)== row:
 				cell.background_color = self.thememanager.running_cell_background_colour
 				cell.text_label.text_color = self.thememanager.running_cell_text_colour
 				cell.detail_text_label.text_color = self.thememanager.running_cell_text_colour
@@ -85,40 +93,50 @@ class FlowCreationView(object):
 				cell.text_label.text_color = self.thememanager.main_text_colour
 				cell.detail_text_label.text_color = self.thememanager.main_text_colour
 			return cell
-		elif row == self.adminRow:
-			cell = ui.TableViewCell()
-			cell.background_color=self.thememanager.main_background_colour
-			cell.selectable = False
-			editButton = ui.Button(title='Done' if tableview.editing else 'Edit')
-			editButton.width *= 1.4
-			editButton.action = swap_edit
-			editButton.y = cell.height/2 - editButton.height/2
-			editButton.x = cell.width
-			cell.add_subview(editButton)
-			self.titleButton.y = cell.height/2 - editButton.height/2
-			self.titleButton.x = self.titleButton.width/2
-			self.titleButton.action = self.change_title
-			cell.add_subview(self.titleButton)
-			return cell
-		elif row == self.saveToHomeScreenRow:
-			cell = ui.TableViewCell()
-			cell.background_color=self.thememanager.main_background_colour
-			cell.selectable = True
-			cell.text_label.text_color = self.thememanager.main_text_colour
-			cell.text_label.text = 'Save To Home Screen'
-			return cell
-		elif row == self.typeRow:
-			cell = ui.TableViewCell('value1')
-			cell.background_color=self.thememanager.main_background_colour
-			cell.selectable = True
-			cell.text_label.text_color = self.thememanager.main_text_colour
-			cell.detail_text_label.text_color = self.thememanager.main_text_colour
-			cell.text_label.text = 'Type of Flow'
-			cell.detail_text_label.text = self.flowType
-			return cell
+		elif section == 0:
+			if row == self.adminRow:
+				cell = ui.TableViewCell()
+				cell.background_color=self.thememanager.main_background_colour
+				cell.selectable = False
+				editButton = ui.Button(title='Done' if tableview.editing else 'Edit')
+				editButton.width *= 1.4
+				editButton.action = swap_edit
+				editButton.y = cell.height/2 - editButton.height/2
+				editButton.x = cell.width
+				cell.add_subview(editButton)
+				self.saveFlowButton.y = cell.height/2 - self.saveFlowButton.height/2
+				self.saveFlowButton.x = self.saveFlowButton.width
+				self.saveFlowButton.action = self.safeFlowAction
+				cell.add_subview(self.saveFlowButton)
+				return cell
+			elif row == self.saveToHomeScreenRow:
+				cell = ui.TableViewCell()
+				cell.background_color=self.thememanager.main_background_colour
+				cell.selectable = True
+				cell.text_label.text_color = self.thememanager.main_text_colour
+				cell.text_label.text = 'Save To Home Screen'
+				return cell
+			elif row == self.typeRow:
+				cell = ui.TableViewCell('value1')
+				cell.background_color=self.thememanager.main_background_colour
+				cell.selectable = True
+				cell.text_label.text_color = self.thememanager.main_text_colour
+				cell.detail_text_label.text_color = self.thememanager.main_text_colour
+				cell.text_label.text = 'Type of Flow'
+				cell.detail_text_label.text = self.flowType
+				return cell
+			elif row == self.titleRow:
+				cell = ui.TableViewCell('value1')
+				cell.background_color=self.thememanager.main_background_colour
+				cell.selectable = True
+				cell.text_label.text_color = self.thememanager.main_text_colour
+				cell.detail_text_label.text_color = self.thememanager.main_text_colour
+				cell.text_label.text = 'Flow Title'
+				cell.detail_text_label.text = self.title
+				return cell
 			
 	@ui.in_background		
-	def change_title(self, sender):
+	def change_title(self):
 		self.oldtitle = self.title
 		self.title = console.input_alert('Flow title','',self.title,'Ok',False)
 		table_view.name = self.title
@@ -127,19 +145,28 @@ class FlowCreationView(object):
 		
 	def tableview_can_delete(self, tableview, section, row):
 		# Return True if the user should be able to delete the given row.
-		return row >= self.extraRows
+		if section == 0:
+			return False
+		elif section == 1:
+			return True
 
 	def tableview_can_move(self, tableview, section, row):
 		# Return True if a reordering control should be shown for the given row (in editing mode).
-		return row >= self.extraRows
+		if section == 0:
+			return False
+		elif section == 1:
+			return True
 
 	def tableview_delete(self, tableview, section, row):
 		# Called when the user confirms deletion of the given row.
-		self.elements.pop(row-self.extraRows)
+		self.elements.pop(row)
 		del_row([row])
 
 	def tableview_move_row(self, tableview, from_section, from_row, to_section, to_row):
-		self.elements.insert(to_row-self.extraRows, self.elements.pop(from_row-self.extraRows))
+		to = to_row
+		if from_section > to_section:
+			to = 0
+		self.elements.insert(to, self.elements.pop(from_row))
 		tableview.reload()
 
 
@@ -168,7 +195,6 @@ def swap_edit(sender):
 def show_edit_buttons():
 	table_view.right_button_items = table_view.data_source.editButtonsRight
 	table_view.left_button_items = table_view.data_source.editButtonsLeft
-	table_view.data_source.titleButton.hidden = False
 
 def show_run_buttons():
 	if table_view.data_source.elements:
@@ -176,7 +202,6 @@ def show_run_buttons():
 	else:
 		table_view.right_button_items = []
 	table_view.left_button_items = table_view.data_source.runButtonsLeft
-	table_view.data_source.titleButton.hidden = True
 		
 def del_row(row):
 	table_view.delete_rows(row)
